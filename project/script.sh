3c1868086f03499a7775f0be69039b20f1cf3f38
deepika2513 <deepikakumar2513@gmail.com>
2024-12-17 12:49:55 +0530

    Add files via upload

---
+++ project/script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo " enter the num1"
+read num1
+echo "enter the num2"
+read num2
+
+sum=$(($num1+$num2))
+echo "the result" $sum
+
---
+++ project/script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter number 1 to 5"
+read n1
+echo "Enter number 6 to 10"
+read n2
+
+if [$n1 -ge $n2]
